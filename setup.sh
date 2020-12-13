efb6282b16670584002561bc1918d7b2b978dabb
moldhouse <moritzalthaus@gmx.de>
2020-12-13 17:20:20 +0100

    Add python build dependencies

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-# gcc
-sudo apt-get install gcc
-sudo apt-get install build-essential
+# python build environment
+sudo apt-get update
+    
+sudo apt-get update; sudo apt-get install --no-install-recommends gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
