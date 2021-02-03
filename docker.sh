7f78a0ce7cee08b37987dfb3573c43b62efa4a4f
Ubuntu <ubuntu@ip-172-31-21-130.eu-central-1.compute.internal>
2021-02-03 11:19:31 +0000

    Split python and docker setup in two files

--- docker.sh
+++ docker.sh
@@ -1,22 +1,3 @@
-# python build environment
-sudo apt-get update
-    
-sudo apt-get update; sudo apt-get install --no-install-recommends gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-# pyenv
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-
-# add pyenv to load path, adjust for zsh
-echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-
-exec $SHELL
-pyenv install -v 3.8.6
-
-# set global python
-pyenv global 3.8.6
-
 # poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 source $HOME/.poetry/env
